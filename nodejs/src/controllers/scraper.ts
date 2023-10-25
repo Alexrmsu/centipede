cf08deca35ed7f208f3f7726c23ddb7f3d68992d
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-10-24 23:50:10 -0300

    enchancement (scrapers directory)

--- nodejs/src/controllers/scraper.ts
+++ nodejs/src/controllers/scraper.ts
@@ -1,8 +1,6 @@
 import {Request, Response} from 'express';
 import Scraper from "../models/scraper";
 
-
-
 export async function getScraper(req: Request, res: Response) {
     const scraper = await Scraper.find();
     return res.json(scraper);
--- nodejs/src/database/connection.ts
+++ nodejs/src/database/connection.ts
@@ -2,6 +2,6 @@ import {connect} from 'mongoose';
 
 
 export async function connection(){
-    connect('mongodb://localhost:27017/dummy', {user: 'dummy', pass:'test' })
+    await connect('mongodb://localhost:27017/dummy', {user: 'dummy', pass: 'test'})
     console.log('Database is connected')
 }
