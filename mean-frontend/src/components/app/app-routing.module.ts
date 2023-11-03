6074a73e579db30d3e527809645e71c7db58dd1e
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-11-02 23:10:06 -0300

    enhancement (no more mongo express and few changes on angular components)

--- mean-frontend/src/components/app/app-routing.module.ts
+++ mean-frontend/src/components/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {LoginComponent} from "../login/login.component";
 const routes: Routes = [
   {path: 'ui/dashboard', component: DashboardComponent},
   {path: 'ui/login', component: LoginComponent},
+  {path: '**', redirectTo: 'ui/dashboard'}
 ];
 
 @NgModule({
