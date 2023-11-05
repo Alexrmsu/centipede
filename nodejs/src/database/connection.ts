23f606af20b43add032d70f6851495b37cd9b5e0
Alexrmsu <al.magna@alumnos.duoc.cl>
2023-11-05 20:40:37 -0300

    feature (jwt validator)

--- nodejs/src/database/connection.ts
+++ nodejs/src/database/connection.ts
@@ -1,7 +1,6 @@
 import {connect} from 'mongoose';
 
-
 export async function connection(){
     await connect('mongodb://localhost:27017/dummy', {user: 'dummy', pass: 'test'})
-    console.log('Database is connected');
+    console.log('Database connection established');
 }
---
+++ nodejs/src/libs/jwt-verify.ts
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken({res, req, next}: { res: any, req: any, next: any }){
+    const token = req.headers['x-access-token'];
+    if(!token){
+        return res.status(401).json({
+            auth: false,
+            message: 'No token provided'
+        });
+    }
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.id;
+    next();
+}
