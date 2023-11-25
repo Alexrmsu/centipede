26da89c04b5c457ae774935ff38bdf2c0cc3468e
Alexrmsu <alexmagnasura@gmail.com>
2023-11-25 18:59:15 -0300

    fix (fixes on scraper services)

--- mean-frontend/src/services/scraper.service.ts
+++ mean-frontend/src/services/scraper.service.ts
@@ -14,15 +14,15 @@ export class ScraperService {
   url : string = 'localhost:3000/api/'
 
   findAll(): Observable<Object> {
-    return this.http.get<Object>(this.url +'scraper/findAll');
+    return this.http.get<Object>(this.url +'findAll');
   }
 
   execute(id: string): Observable<Object> {
-    return this.http.get<Object>(this.url + 'scraper/execute/' + id);
+    return this.http.get<Object>(this.url + 'execute/' + id);
   }
 
   createScraper(path: string, source: string, tech: string): Observable<Object> {
-    return this.http.post<Object>(this.url + 'scraper/create', {path, source, tech});
+    return this.http.post<Object>(this.url + 'create', {path, source, tech});
   }
 
 
