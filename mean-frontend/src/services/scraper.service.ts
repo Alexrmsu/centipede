780ea1cf50700ba68aa8c4bcb8d2c8556a0ac1a2
Alexrmsu <alexmagnasura@gmail.com>
2023-12-03 04:59:51 -0300

    small fix (uncommented createScraper void)

--- mean-frontend/src/services/scraper.service.ts
+++ mean-frontend/src/services/scraper.service.ts
@@ -10,7 +10,7 @@ export class ScraperService {
   constructor(
     private http : HttpClient,
   ) {}
-  url : string = 'localhost:3000/api/'
+  url : string = 'http://localhost:3000/api/'
 
   findAll(): Observable<Object> {
     return this.http.get<Object>(this.url +'findAll');
@@ -19,12 +19,9 @@ export class ScraperService {
   execute(id: string): Observable<Object> {
     return this.http.get<Object>(this.url + 'execute/' + id);
   }
-  /*
+
   createScraper(path: string, source: string, tech: string): Observable<Object> {
     return this.http.post<Object>(this.url + 'create', {path, source, tech});
   }
-  */
-
-
 
 }
