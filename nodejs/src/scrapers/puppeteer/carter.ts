debed98744492d28b319bfca06eb640d389ed016
Alexrmsu <alexmagnasura@gmail.com>
2024-01-14 05:05:09 -0300

    feature (first test with US site)

--- nodejs/src/scrapers/puppeteer/carter.ts
+++ nodejs/src/scrapers/puppeteer/carter.ts
@@ -1,12 +1,30 @@
-import {getHTML} from "./libs/common";
 import puppeteer from 'puppeteer';
+import bodyParser from 'body-parser';
 
 const baseUrl = 'https://cartercountysheriff.us/inmates2.html';
 const sourceName = "Carter County Sheriff's Office";
 
-const scrapeInmate = async (url: string) => {
-    const html = await getHTML(url);
-    return html;
+const getInmates = async () => {
+    let inmates = [];
+
+    const browser = await puppeteer.launch({
+        headless: true,
+        defaultViewport: null,
+    });
+
+    const page = await browser.newPage();
+
+    await page.goto(baseUrl, {
+        waitUntil: "domcontentloaded",
+    });
+
+    let inmateName = await page.$x('//tr[@class="RowStyle"]//td[2]/span/text()');
+
+
+
+
+    // all inmateNames
+    console.log(inmateName);
+    await browser.close();
 };
-
-
+getInmates();
