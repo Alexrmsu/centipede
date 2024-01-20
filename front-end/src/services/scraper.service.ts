7b620c1391b670088f36ed9350aeac9caa343063
Alexrmsu <alexmagnasura@gmail.com>
2024-01-20 13:35:25 -0300

    enhancements (replacing key variables with environment files)

--- front-end/src/services/scraper.service.ts
+++ front-end/src/services/scraper.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
+import {API_URL} from "../environments/environment.development";
 
 @Injectable({
   providedIn: 'root'
@@ -10,18 +11,6 @@ export class ScraperService {
   constructor(
     private http : HttpClient,
   ) {}
-  url : string = 'http://localhost:3000/api/'
-
-  findAll(): Observable<Object> {
-    return this.http.get<Object>(this.url +'findAll');
-  }
-
-  execute(id: string): Observable<Object> {
-    return this.http.get<Object>(this.url + 'execute/' + id);
-  }
-
-  createScraper(path: string, source: string, tech: string): Observable<Object> {
-    return this.http.post<Object>(this.url + 'create', {path, source, tech});
-  }
+  url : string = API_URL + 'scraper/';
 
 }
