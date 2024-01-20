4aed8c00adfc2f5aef69c3d84dfbfc9ed68f51ac
Alexrmsu <alexmagnasura@gmail.com>
2024-01-20 03:03:38 -0300

    enhancements (creating base database structure)

--- nodejs/src/database/start.sql
+++ nodejs/src/database/start.sql
@@ -1,1 +1,28 @@
 create schema if not exists scrapers;
+
+create table if not exists scraper(
+    id serial primary key auto_increment,
+    path varchar(255) not null,
+    source varchar(255) not null,
+    tech varchar(255) not null,
+    created_at timestamp default current_timestamp,
+    updated_at timestamp default current_timestamp on update current_timestamp
+);
+
+create table if not exists workerStatus(
+    id serial primary key auto_increment,
+    workerName varchar(255) not null,
+    status varchar(255) not null,
+    scraperId int not null,
+    run_date timestamp default current_timestamp,
+    update_at timestamp default current_timestamp on update current_timestamp
+);
+
+create table if not exists person
+(
+    id      serial primary key auto_increment,
+    name    varchar(255) not null,
+    email   varchar(255),
+    phone   varchar(255),
+    address varchar(255)
+)
