9c23b4468f820e039f9bea619046b80fda824d0a
Alexrmsu <alexmagnasura@gmail.com>
2024-03-12 20:22:26 -0300

    features (adding updateContextValues function)

--- nodejs/src/scrapers/puppeteer/libs/context.ts
+++ nodejs/src/scrapers/puppeteer/libs/context.ts
@@ -10,7 +10,16 @@ export interface Context {
 
 export const getContexts = async (scraperId: number): Promise<Context[]> => {
     return new Promise((resolve, reject) => {
-        connection.query('SELECT * FROM context WHERE scraperId = ?', [scraperId], (err, results) : void => {
+        connection.query('SELECT * FROM context WHERE scraperId = ?', [scraperId], (err: Error, results : Context[]) : void => {
+            if(err) reject(err);
+            resolve(results);
+        });
+    });
+}
+
+export const updateContextValues = async (id: number, contextValues: string): Promise<Context> => {
+    return new Promise((resolve, reject) => {
+        connection.query('UPDATE context SET contextValues = ? WHERE id = ?', [contextValues, id], (err: Error, results : Context) : void => {
             if(err) reject(err);
             resolve(results);
         });
