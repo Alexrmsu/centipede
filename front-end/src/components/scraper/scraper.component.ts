db9984bcfde720ac4374194b1a51facc48b974da
Alexrmsu <alexmagnasura@gmail.com>
2024-04-07 16:34:05 -0400

    front-end update

--- front-end/src/components/scraper/scraper.component.ts
+++ front-end/src/components/scraper/scraper.component.ts
@@ -1,39 +1,39 @@
 import { LiveAnnouncer } from '@angular/cdk/a11y';
-import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Scraper } from 'src/interfaces/scraper';
 import { ScraperService } from 'src/services/scraper.service';
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-};
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
 
 @Component({
   selector: 'app-scraper',
   templateUrl: './scraper.component.html',
   styleUrls: ['./scraper.component.css']
 })
-export class ScraperComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = new MatTableDataSource(ELEMENT_DATA);
+export class ScraperComponent implements OnInit, AfterViewInit{
+  displayedColumns: string[] = ['id', 'path', 'source', 'tech', 'dateCreated', 'dateUpdated'];
+  dataSource = new MatTableDataSource<Scraper[]>;
+  actualScraper : FormGroup | undefined;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+
+
 
   constructor(
      private scraperService: ScraperService,
   ) {}
+
+  ngOnInit() : void  {
+    this.scraperService.getScrapers();
+  }
+
+  ngAfterViewInit(): void {
+    this.dataSource.sort = this.sort;
+  }
+
+
+
 
 }
