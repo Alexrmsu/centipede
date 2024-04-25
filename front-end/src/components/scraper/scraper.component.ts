e0ce148521b7cd29891bef4acf2fae329e746add
Alexrmsu <alexmagnasura@gmail.com>
2024-04-24 23:31:04 -0400

    Front-end scraper rework

--- front-end/src/components/scraper/scraper.component.ts
+++ front-end/src/components/scraper/scraper.component.ts
@@ -1,25 +1,40 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { MatSort, Sort } from '@angular/material/sort';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Scraper } from 'src/interfaces/scraper';
 import { ScraperService } from 'src/services/scraper.service';
 import Swal from 'sweetalert2';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
 
 @Component({
   selector: 'app-scraper',
   templateUrl: './scraper.component.html',
-  styleUrls: ['./scraper.component.css']
+  styleUrls: ['./scraper.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0', display: 'none' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class ScraperComponent implements OnInit, AfterViewInit{
   displayedColumns: string[] = ['id', 'path', 'source', 'tech', 'dateUpdated', 'dateCreated'];
+  displayedColumnsExpand = ['id', 'path', 'source', 'tech', 'dateUpdated', 'dateCreated'];
   dataSource = new MatTableDataSource<Scraper>;
+  expandedElement: Scraper | null | undefined;
   actualScraper : FormGroup | undefined;
   @ViewChild(MatSort) sort!: MatSort;
 
   constructor(
      private scraperService: ScraperService,
-  ) {}
+  ) { }
+
+  toggleExpansion(row : any): void {
+    this.expandedElement = this.expandedElement === row ? null : row;
+  }
 
   ngOnInit() : void  {
     this.getScrapers();
