29bbf506f8f0e4ee0c36311061450b619f548d6d
Alexrmsu <alexmagnasura@gmail.com>
2024-06-09 23:49:52 -0400

    scraper details component

--- nodejs/src/controllers/scraper.ts
+++ nodejs/src/controllers/scraper.ts
@@ -48,7 +48,7 @@ export async function createScraper(req: Request, res: Response) {
 }
 
 export async function testCreateFile(req: Request, res: Response) {
-    const {path,source,tech} = {path: 'src/scrapers/puppeteer/pcfactory', source: 'test', tech: ['test']}
+    const {path,source ,tech  } = {path: 'src/scrapers/puppeteer/pcfactory', source: 'test', tech: ['test']}
     const pathFile : string = path.match("puppeteer") ? path + '.ts' : path + '.py';
     if (fs.existsSync(pathFile)) {
         return res.json({
